36baf55c39636b2288989e3a608638d7ca522a9b
Faatima Abdirahmaan Ali <faatimaapdirahmaanali@gmail.com>
2024-08-31 08:56:59 +0300

    age calculator

---
+++ src/JOptionpane.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+class JOptionpane {
+
+    static void showMessageDialog(Object object, NumberFormatException error) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
